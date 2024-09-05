59d536f8a6a70045a8310e379b52d69960a5362b
Hanru Vorster <hanru.vorster@gmail.com>
2024-09-05 14:08:40 +0200

    Fetching data from CoinGeckoAPI for DogeCoin and logging the data to the console

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ fetch(
   .catch((err) => {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1547234935-80c7145ec969?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjU1MzcxNTR8&ixlib=rb-4.0.3&q=85)`;
   });
+
+//Fetching data from CoinGeckoAPI
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
