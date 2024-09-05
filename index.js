80cdb44f5b95419d1d0e444123b46b91e7d50f21
Hanru Vorster <hanru.vorster@gmail.com>
2024-09-05 14:42:44 +0200

    added coin stats to crypto div

--- index.js
+++ index.js
@@ -31,5 +31,10 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     <img src= ${data.image.thumb}
     <span>${data.name}</span>
     `;
+    document.getElementById("crypto").innerHTML += `
+    <p>🎯R ${data.market_data.current_price.zar}</p>
+    <p>👆R ${data.market_data.high_24h.zar}</p>
+    <p>👇R ${data.market_data.low_24h.zar}</p>
+    `;
   })
   .catch((err) => console.error(err));
