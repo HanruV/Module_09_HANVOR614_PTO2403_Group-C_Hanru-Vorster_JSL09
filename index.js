5d729cd227dc7fb39b84116ea471470b13969268
Hanru Vorster <hanru.vorster@gmail.com>
2024-09-05 13:55:35 +0200

    added a catch error handler to diplsay a default image if promise rejects

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ fetch(
 )
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
+    console.log(data.urls.full);
     document.body.style.backgroundImage = `url(${data.urls.full})`;
     //Adding author name
     document.getElementById(
       "author"
     ).textContent = `Img Author: ${data.user.name}`;
+  })
+  .catch((err) => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1547234935-80c7145ec969?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjU1MzcxNTR8&ixlib=rb-4.0.3&q=85)`;
   });
