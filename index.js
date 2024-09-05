830087d865699e6a6c5b50b12c3e5f447494ce49
Hanru Vorster <hanru.vorster@gmail.com>
2024-09-05 13:07:55 +0200

    added img author to the dom and styled with css

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+//Fetching a random image from unsplash and setting
+//as background for body
 fetch(
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
 )
   .then((res) => res.json())
   .then((data) => {
+    console.log(data);
     document.body.style.backgroundImage = `url(${data.urls.full})`;
+    document.getElementById("author").textContent = data.user.name;
   });
